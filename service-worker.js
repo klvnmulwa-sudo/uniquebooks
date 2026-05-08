4512f75eaa3bad3289ff52195d4b8ad79c302467
klvnmulwa-sudo <klvnmulwa@gmail.com>
2026-05-08 11:17:41 +0300

    app

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'unique-books-v1';
+self.addEventListener('install', function(e) { self.skipWaiting(); });
+self.addEventListener('activate', function(e) { e.waitUntil(caches.keys().then(function(names) { return Promise.all(names.filter(function(n) { return n !== CACHE_NAME; }).map(function(n) { return caches.delete(n); })); })); });
+self.addEventListener('fetch', function(e) { e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request).then(function(res) { var clone = res.clone(); caches.open(CACHE_NAME).then(function(c) { c.put(e.request, clone); }); return res; }); })); });
